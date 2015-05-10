1823ef4cde57fb59688051ef00cf837a82a9e832
ratacat <neuralsplash@gmail.com>
2015-05-09 20:06:53 -0700

    created Game.addPlayer function, using a automatic player list

--- app.js
+++ app.js
@@ -1,71 +1,92 @@
-// OOP Tic Tac Toe boilerplate code
+//on window load start the game
+$(document).ready (function(){
 
-// Execute this code only AFTER the document is ready
-// Hint: use jQuery's `$(document).ready`
+   //and we're off!
+  game = new Game();
+  
+  //lets get a few players
+   p1 = game.addPlayer("John","X");
+   p2 = game.addPlayer("Ruthless Mercenary","R");
+
+   //decide who goes first
+   game.firstTurn = game.players[Math.round(Math.random()) ];
+
+   
+});
+
+
 
   function Game() {
-    //Create a new instance of player 1
-    //this.player1 = ...
-
-    //Do the same for a player 2
-    //this.player2 = ...
+    this.board = this.loadBoard();
     
-    //Create the board
-    //this.board = ...
+    this.players = [];
+    this.moves = [];
+
+
   }
 
   // Remember: prototypes are shared functions between all game instances
-  Game.prototype.nextPlayer = function() {
-    //Switch players
+  Game.prototype.nextMove = function() {
+    //check to see if firstmove is set
+    //pick a player and start with them
+    //check whether requested move is open
+    //record move in this.board
+    //alternate based on movement record
   };
 
-  // `Game.prototype.init` kicks off a new game with a board and two players
-  Game.prototype.init = function() {
-    //
+  // Game.prototype.init kicks off a new game with a board and two players
+  Game.prototype.loadBoard = function() {
+      return new Board();
   };
+
+  Game.prototype.addPlayer = function(name,symbol) {
+     player = new Player(name,symbol);
+     this.players.push(player);
+    return player;
+  }
+
+  Game.prototype.move = function(){
+    console.log("Game.move fired!");
+  }
 
   // A starter Player constructor.
-  function Player(team) {
+  function Player(name,piece) {
+      this.name = name;
+      this.piece = piece;
     //Is the player X or O?
     //this.team = ...
   };
 
   // A starter Board constructor.
   function Board() {
-      this.squares = [];
+      this.init();
 
-    //Tracks the cells of the board instance
-    //this.$cells = ...
-
-    //Store any other properties that board may have below, such as a reset option
   };
 
+//builds the board with jquery and inserts
+//it into body tag of document
 Board.prototype.init = function() {
-      //build board
       var $board = $('<div id="board">');
 
       for (i=0;i<9;i++){
-            $board.append('<div class="square">');
+            $board.append('<div id="square'+i+'"" class="square">');
       }
+       
 
       $('body').append($board);
-      console.log($board);
+
+        // oneClick();
+      this.$squares = $('.square');
+      this.$squares.click(function(e){
+        game.move();
+        //console.log("Click on #" +e.target.id);
+      });
+
 }
 
+//removes the board from dom document
 Board.prototype.destroy = function() {
      var $board = $('#board');
      console.log($board);
      $board.remove();
 }
-
-//on window load
-$(document).ready (function(){
-
-  //temporary driver
-  b = new Board();
-
-  // Start the game!
-  var game = new Game();
-  game.init();
-
-});
